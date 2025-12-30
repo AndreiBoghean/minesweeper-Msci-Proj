846cf938c431734ace2454e418aa27d8afdeefb3
AndreiBoghean <andreiboghean00@gmail.com>
2025-12-30 04:10:12 +0000

    added assets for other cell types and subsequent support for rendering ; almost finished main interaction handling

--- experimentPlatform/minesweeperWeb/main.js
+++ experimentPlatform/minesweeperWeb/main.js
@@ -13,9 +13,13 @@ function sleep(milliseconds) {
  * generates hints, and passes to interaction listeners somehow
  * registers interaction listeners -> interaction listeners do some telemetry
 */
+
+// NOTE: we're only running the experiment with 9x9 fields, but we'll leave groundwork for other fields so I can make this a fully fleshed out web game outwith the dissertation.
+let fieldWidth = 9;
+let fieldHeight = 9;
 let mineLayout = mineGen();
 let mineHints = hintGen(mineLayout);
-
+let startTime = 0 // TODO: need to link this with start button
 
 /* on each render, we first draw the base board
  * then we draw the mines remaining, restart button, timer
@@ -23,19 +27,37 @@ let mineHints = hintGen(mineLayout);
  *
  * grid drawing is either: 0. a closed cell, 1. a closed flagged cell, 2. a numbered open cell with hint 0-9 (0 renders no number), 3. a mine (or explosion)
 */
-// prepare the starting field. note: the field is a 9x9 grid.
-let fieldState = Array(9).fill(Array(9).fill(0)); // filled with code 0 for closed cells.
+// prepare the starting field.
+let fieldState = []
+for (let _ = 0; _ < fieldHeight ; _++) {
+    fieldState.push(Array(fieldWidth).fill(0))
+}
+
+console.log("fielderfield");
+console.log(fieldState);
+
 
 // prepare necessary assets
-await preloadURL("./assets/closedCell.png", "closedCell");
 await preloadURL("./assets/background.png", "background");
+await preloadURL("./assets/closedCell.png", "closedCell"); // field ID 0
+await preloadURL("./assets/flaggedCell.png", "flaggedCell"); // field ID 1
+await preloadURL("./assets/hint0Cell.png", "hint0Cell"); // field ID 2
+await preloadURL("./assets/hint1Cell.png", "hint1Cell"); // field ID 2
+await preloadURL("./assets/hint2Cell.png", "hint2Cell"); // field ID 2
+await preloadURL("./assets/hint3Cell.png", "hint3Cell"); // field ID 2
+await preloadURL("./assets/hint4Cell.png", "hint4Cell"); // field ID 2
+await preloadURL("./assets/hint5Cell.png", "hint5Cell"); // field ID 2
+await preloadURL("./assets/hint6Cell.png", "hint6Cell"); // field ID 2
+await preloadURL("./assets/hint7Cell.png", "hint7Cell"); // field ID 2
+await preloadURL("./assets/hint8Cell.png", "hint8Cell"); // field ID 2
+await preloadURL("./assets/mineCell.png", "mineCell"); // field ID 3
 
 // render the first state of the game.
-renderGame(gameCtx, fieldState, mineHints, 0, 9, 9);
+renderGame(gameCtx, fieldState, mineLayout, mineHints, startTime, 9, 9);
 // note that subsequent game states happen as a result of user actions,
 // hence the processing and subsequent rendering of future game states happens from there.
 
 
 // TODO: register user action listeners
-gameCanv.addEventListener('mouseup', interactHandler);
+gameCanv.addEventListener('mouseup', (e) => { return interactHandler(e, gameCtx, fieldState, mineLayout, mineHints, startTime, 9, 9) });
 // gameCanv.addEventListener('touchend', interactHandler); // relic from touch support.. apprently touches issue both touchend and mouseup? doesnt make sense, but convientient.
--- experimentPlatform/minesweeperWeb/modules/game.js
+++ experimentPlatform/minesweeperWeb/modules/game.js
@@ -24,8 +24,28 @@ export function hintGen(mineLayout) {
             [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 }
 
-function actionResolve(fieldState, x, y) {
+function actionResolve(ctx, fieldState, mineLayout, mineHints, startTime, fieldWidth, fieldHeight, action, x, y) {
+    console.log("action came in")
+    console.log("test:", JSON.stringify(fieldState))
     // TODO: different outcomes according to left click, right click, mine underneath, safe underneath, etc.
+    // TODO: long press for flagging on mobile?
+
+    if (action == 0 && fieldState[y][x] != 1) { // if left click for a reveal action, AND the cell isnt flagged (note we disable opening of flagged cells)
+        // fieldState[y][x] = (mineLayout[y][x] == 1) ? 3 : 2 // if there's a mine, the cell state is now mine (3), otherwise open cell (2)
+        var temp = (mineLayout[y][x] == 1) ? 3 : 2 // if there's a mine, the cell state is now mine (3), otherwise open cell (2)
+    }
+    else if (action == 1) { // right click for a toggle flag
+        // fieldState[y][x] = fieldState[y][x] == 1 ? 0 : 1; // toggle flag state of the cell.
+        var temp = fieldState[y][x] == 1 ? 0 : 1; // toggle flag state of the cell.
+    }
+    fieldState[y][x] = temp;
+    console.log("to update:", fieldState[y][x])
+    console.log("temp", temp)
+    console.log("2est:", JSON.stringify(fieldState))
+
+    console.log("action updated")
+    renderGame(ctx, fieldState, mineLayout, mineHints, startTime, fieldWidth, fieldHeight);
+    console.log("rerendered")
 }
 
 
@@ -33,23 +53,42 @@ function actionResolve(fieldState, x, y) {
 const gridStartX = 24, gridStartY = 110; // note: x:25, y:110 (with 0-based indexing) are the positions for the top-left pixel of the top-left-most cell in our background image.
 const gridSquareSize = 32; // the pixel dimensions of a cell for us right now is 32x32.
 
-export function renderGame(ctx, fieldState, mineHints, startTime, fieldWidth, fieldHeight) {
+export function renderGame(ctx, fieldState, mineLayout, mineHints, startTime, fieldWidth, fieldHeight) {
     placePreloaded(ctx, "background", 0, 0);
 
-    for (let cellY = gridStartY ; cellY < gridStartY+fieldHeight*gridSquareSize ; cellY += gridSquareSize) {
-        for (let cellX = gridStartX ; cellX < gridStartX+fieldWidth*gridSquareSize; cellX += gridSquareSize) {
-            placePreloaded(ctx, "closedCell", cellX, cellY);
+    for (let y = gridStartY ; y < gridStartY+fieldHeight*gridSquareSize ; y += gridSquareSize) {
+        for (let x = gridStartX ; x < gridStartX+fieldWidth*gridSquareSize; x += gridSquareSize) {
+            let [cellX, cellY] = pixelToCell(x, y);
+            switch (fieldState[cellY][cellX]) {
+                case 0:
+                    placePreloaded(ctx, "closedCell", x, y);
+                    break;
+                case 1:
+                    placePreloaded(ctx, "flaggedCell", x, y);
+                    break;
+                case 2:
+                    placePreloaded(ctx, "hint" + mineHints[cellX][cellY] + "Cell", x, y);
+                    break;
+                case 3:
+                    placePreloaded(ctx, "mineCell", x, y);
+                    break;
+                default:
+                    console.log("how did this even happen? comaplain to the dev :)");
+                    console.log(fieldState)
+                    console.log(cellX, cellY);
+                    console.log(fieldState[cellY][cellX])
+                    break;
+            }
         }
     }
 }
 
 function pixelToCell(x, y) {
-    console.log(x, y)
     x = Math.floor((x - gridStartX)/gridSquareSize)
     y = Math.floor((y - gridStartY)/gridSquareSize)
     return [x, y]
 }
-export function interactHandler(e, ctx, fieldState) {
+export function interactHandler(e, ctx, fieldState, mineLayout, mineHints, startTime, fieldWidth, fieldHeight) {
     if (e.type == "mouseup") {
         var currentX = e.offsetX;
         var currentY = e.offsetY;
@@ -63,7 +102,10 @@ export function interactHandler(e, ctx, fieldState) {
 
     const [x, y] = pixelToCell(currentX, currentY);
     console.log(x, y)
+    console.log(e)
 
-    actionResolve(fieldState, x, y);
+    // TODO: this assumes mouse.. touch wont support flagging right now.
+    if (e.button == 0 || e.button == 2) actionResolve(ctx, fieldState, mineLayout, mineHints, startTime, fieldWidth, fieldHeight, e.button == 0 ? 0 : 1, x, y);
+    // ^ note: we ignore events that arent left or right click
 }
 
