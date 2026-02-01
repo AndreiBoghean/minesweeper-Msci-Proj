07b91d4420030c79bac643865d11f8bccebd0543
AndreiBoghean <andreiboghean00@gmail.com>
2026-02-01 14:55:54 +0000

    working GAC

--- experimentPlatform/analyser/proofOfConcept.py
+++ experimentPlatform/analyser/proofOfConcept.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.signal import convolve2d
+import copy
 # import curses
 
 # stdscr = curses.initscr()
@@ -135,7 +136,7 @@ testCasePAPERv2 = np.array([
     [9, 9, 9, 9, 9, 2, 0, 0, 0, 0, 0, 1, 9, 9, 1, 0]
 ])
 
-hints = testCasePAPERv2
+hints = testCasePAPERv1
 
 print("hints:")
 print(hints)
@@ -377,9 +378,9 @@ print("TODO:")
 print(todo)
 
 def consistencyCheck(testableLabel, victimVariable, supportVariables, constraint):
-    print(f"conistency check on {testableLabel=}, {victimVariable=}, {supportVariables=}, {constraint=}")
+    # print(f"conistency check on {testableLabel=}, {victimVariable=}, {supportVariables=}, {constraint=}")
     for acceptedAssignment in constraints[constraint]:
-        print("checking assignment", acceptedAssignment)
+        # print("checking assignment", acceptedAssignment)
         if acceptedAssignment[victimVariable] != testableLabel:
             continue
 
@@ -389,7 +390,7 @@ def consistencyCheck(testableLabel, victimVariable, supportVariables, constraint
             requiredLabel = acceptedAssignment[supVar] # get the label that this assignment requires for this variable
             if not domains[supVar][requiredLabel]:
                 supportable = False
-                print(f"not supportable since {supVar} doesnt have label {requiredLabel}")
+                # print(f"not supportable since {supVar} doesnt have label {requiredLabel}")
                 break
 
         if supportable: return True
@@ -397,6 +398,8 @@ def consistencyCheck(testableLabel, victimVariable, supportVariables, constraint
     return False;
 
 # exit()
+
+newDomains = copy.deepcopy(domains)
 
 # ATTEMPT ONE: implementing algorithm from https://artint.info/3e/html/ArtInt3e.Ch4.S3.html
 while len(todo) > 0: # line 3
@@ -406,7 +409,7 @@ while len(todo) > 0: # line 3
 
     #line 5
     todoYs = [otherVar for otherVar in constraintsToVariables[todoc] if otherVar != todoX]
-    print("for", todoable, "we get Ys", todoYs)
+    # print("for", todoable, "we get Ys", todoYs)
 
     # line 6 and 7
     """
@@ -421,7 +424,8 @@ while len(todo) > 0: # line 3
             acceptedLabels.append(False) # record the label as not supported
 
     # line 8
-    if acceptedLabels != domains[todoX]:
+    # if acceptedLabels != domains[todoX]:
+    if acceptedLabels != domains[todoX] and acceptedLabels != newDomains[todoX]:
         # line 9
         """
         for this step we need every variable Z such that Z is connected to X as a neighbour through a constraint c'
@@ -433,21 +437,97 @@ while len(todo) > 0: # line 3
                     todo.add((todoY, cP))
 
         # line 10
-        domains[todoX] = acceptedLabels
+        # domains[todoX] = acceptedLabels
+        newDomains[todoX] = acceptedLabels
 
         print(f"domains changed! on {todoX}: {domains[todoX]} went to {acceptedLabels}")
         print("new domains:")
         # print(domains)
-        renderDomains(domains)
+        # renderDomains(domains)
+        renderDomains(newDomains)
         print()
 
     # print("new todo:")
     # print(todo)
     # print()
     # print()
-
-print("final domain:")
+domains = newDomains
+print("domains after one attempt:")
 renderDomains(domains)
+
+
+
+exit()
+########################## AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa
+
+# todo is a list of variables that are potentially not arc consistent w.r.t. to a constraint.
+todo = set()
+
+# todo is (initially) a list of every pairing between a variable and a constraint that acts upon it.
+for variable in domains: # remember that domains is dictionary between {variable: domainSet}
+    if variable in variableToConstraints: # if the variable has a constraint (unconstrained variables currenly comprise cells that arent surrounded by any hints)
+        for constraint in variableToConstraints[variable]:
+            todo.add((variable, constraint))
+
+newDomains = copy.deepcopy(domains)
+
+# ATTEMPT ONE: implementing algorithm from https://artint.info/3e/html/ArtInt3e.Ch4.S3.html
+while len(todo) > 0: # line 3
+    # line 4
+    todoable = todo.pop()
+    todoX, todoc = todoable
+
+    #line 5
+    todoYs = [otherVar for otherVar in constraintsToVariables[todoc] if otherVar != todoX]
+    # print("for", todoable, "we get Ys", todoYs)
+
+    # line 6 and 7
+    """
+    for this step we need all the labels on todoX which have a support across variables todoYs,
+    according to constraint todoc.
+    """
+    acceptedLabels = [] # NOTE: variable naming: "accepted labels" actually just means variable domains re-adusted according to what labels are arc consistent
+    for xLabelI in range(len(domains[todoX])):
+        if domains[todoX][xLabelI] and consistencyCheck(xLabelI, todoX, todoYs, todoc): # if the label is still enabled in the domain, and the label meets the consistency check..
+            acceptedLabels.append(True) # record the variable as supported
+        else:
+            acceptedLabels.append(False) # record the label as not supported
+
+    # line 8
+    # if acceptedLabels != domains[todoX]:
+    if acceptedLabels != domains[todoX] and acceptedLabels != newDomains[todoX]:
+        # line 9
+        """
+        for this step we need every variable Z such that Z is connected to X as a neighbour through a constraint c'
+        ^ subject to c' != c and Z != X
+        """
+        for cP in variableToConstraints[todoX]: # for each constraint connected to X
+            for todoY in constraintsToVariables[cP]: # for each variable connected to that constraint
+                if todoY != todoX and cP != todoc:
+                    todo.add((todoY, cP))
+
+        # line 10
+        # domains[todoX] = acceptedLabels
+        newDomains[todoX] = acceptedLabels
+
+        print(f"domains changed! on {todoX}: {domains[todoX]} went to {acceptedLabels}")
+        print("new domains:")
+        # print(domains)
+        # renderDomains(domains)
+        renderDomains(newDomains)
+        print()
+
+    # print("new todo:")
+    # print(todo)
+    # print()
+    # print()
+domains = newDomains
+print("domains after TWO attempt:")
+renderDomains(domains)
+
+
+
+
 """
 remember - AC3,4 only operates over binary constraints.
 we've implemented this model directly with multi-varaible constraints (specifically, constraints with 8 variables, each of its neighbours)
